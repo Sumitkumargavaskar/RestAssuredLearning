e180f08c48bddd6c88c0a0298881b169e00c74d1
Sumitkumargavaskar <sumitkumargavaskar@gmail.com>
2025-02-01 03:05:50 +0530

    FirstRestAssuredProgram

---
+++ src/test/java/com/thetestingacademy/testng/Lab261.java
@@ -0,0 +1,28 @@
+package com.thetestingacademy.testng;
+
+import io.qameta.allure.Description;
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
+public class Lab261 {
+    @Description("TC#1 - Verify  261 Get Request 1")
+    @Test
+    public void getRequest() {
+        RestAssured
+                .given()
+                .baseUri("https://restful-booker.herokuapp.com/")
+                .basePath("/ping")
+                .when().get().then().statusCode(201);
+
+    }
+    @Description("TC#1 - Verify 261 Get Request 2")
+    @Test
+    public void getRequest2() {
+        RestAssured
+                .given()
+                .baseUri("https://restful-booker.herokuapp.com/")
+                .basePath("/ping")
+                .when().get().then().statusCode(201);
+
+    }
+}
